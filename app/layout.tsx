9dfc5249d7364d279ec3b1129187451e9d382e30
Dhruv Anand <ananddhruv29@gmail.com>
2024-10-20 02:49:10 +0530

    Changed font
    Switched t Google's Sora font.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import { Sora } from "next/font/google";
 import "./globals.css";
 
 const geistSans = localFont({
@@ -11,6 +12,9 @@ const geistMono = localFont({
 	src: "./fonts/GeistMonoVF.woff",
 	variable: "--font-geist-mono",
 	weight: "100 900",
+const SoraFont = Sora({
+	subsets: ["latin"],
+	weight: "400",
 });
 
 export const metadata: Metadata = {
@@ -27,6 +31,7 @@ export default function RootLayout({
 		<html lang="en">
 			<body
 				className={`${geistSans.className} ${geistMono.className} min-h-screen antialiased`}>
+			<body className={`${SoraFont.className} min-h-screen antialiased`}>
 				{children}
 			</body>
 		</html>
