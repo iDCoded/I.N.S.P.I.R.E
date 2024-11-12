ab20ca904d86e0a82a445aa1c0d3705cbff0e8b5
Dhruv Anand <ananddhruv29@gmail.com>
2024-11-12 13:43:01 +0530

    Setup context to schedule ticket

---
+++ context/poll-provider.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { createContext, ReactNode, useContext, useState } from "react";
+
+const BusScheduleContext = createContext<BusScheduleContextType | undefined>(
+	undefined
+);
+
+export const BusScheduleProvider = ({ children }: { children: ReactNode }) => {
+	const [selectedTime, setSelectedTime] = useState("");
+	const [ticket, setTicket] = useState<Ticket | null>(null);
+
+	const generateTicket = () => {
+		if (selectedTime) {
+			const ticketId = Math.random()
+				.toString(36)
+				.substring(5, 10)
+				.toUpperCase();
+			setTicket({ time: selectedTime, ticketId });
+		}
+	};
+
+	return (
+		<BusScheduleContext.Provider
+			value={{
+				selectedTime,
+				setSelectedTime,
+				ticket,
+				generateTicket,
+			}}>
+			{children}
+		</BusScheduleContext.Provider>
+	);
+};
+
+export const useBusSchedule = () => {
+	const context = useContext(BusScheduleContext);
+	if (!context) {
+		throw new Error("useBusSchedule must be used within a BusScheduleProvider");
+	}
+	return context;
+};
