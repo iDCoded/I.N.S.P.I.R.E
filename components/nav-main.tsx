125fcb6b5d279124cd96c64ccc2b9fa93ace782b
Dhruv Anand <ananddhruv29@gmail.com>
2024-11-12 10:48:17 +0530

    Update nav-main.tsx

--- components/nav-main.tsx
+++ components/nav-main.tsx
@@ -1,38 +1,38 @@
-"use client"
+"use client";
 
-import { type LucideIcon } from "lucide-react"
+import { type LucideIcon } from "lucide-react";
 
 import {
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
-} from "@/components/ui/sidebar"
-import Link from "next/link"
+	SidebarMenu,
+	SidebarMenuButton,
+	SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import Link from "next/link";
 
 export function NavMain({
-  items,
+	items,
 }: {
-  items: {
-    title: string
-    url: string
-    icon: LucideIcon
-    isActive?: boolean
-  }[]
+	items: {
+		title: string;
+		url: string;
+		icon: LucideIcon;
+		isActive?: boolean;
+	}[];
 }) {
-  return (
-    <SidebarMenu>
-      {items.map((item) => (
-        <Link href={item.url}>
-          <SidebarMenuItem key={item.title}>
-            <SidebarMenuButton asChild isActive={item.isActive}>
-              <a href={item.url}>
-                <item.icon />
-                <span>{item.title}</span>
-              </a>
-            </SidebarMenuButton>
-          </SidebarMenuItem>
-        </Link>
-      ))}
-    </SidebarMenu>
-  )
+	return (
+		<SidebarMenu>
+			{items.map((item) => (
+				<Link key={item.title} href={item.url}>
+					<SidebarMenuItem>
+						<SidebarMenuButton asChild isActive={item.isActive}>
+							<a href={item.url}>
+								<item.icon />
+								<span>{item.title}</span>
+							</a>
+						</SidebarMenuButton>
+					</SidebarMenuItem>
+				</Link>
+			))}
+		</SidebarMenu>
+	);
 }
