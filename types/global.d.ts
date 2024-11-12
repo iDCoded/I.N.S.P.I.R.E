2a105793adbb4733d639bdc603ec497e692d42ba
Dhruv Anand <ananddhruv29@gmail.com>
2024-11-12 16:36:33 +0530

    Updated `_id` type

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,7 +1,7 @@
-import { Document } from "mongoose";
+import { Document, Types } from "mongoose";
 
 interface IPoll extends Document {
-	_id: number;
+	_id: Types.ObjectId;
 	name: string;
 	timings: string[];
 	status: "pending" | "active" | "ended";
