1eb4c3876c4b87c071b4d96942535e311c5d13f7
Dhruv Anand <ananddhruv29@gmail.com>
2024-11-12 13:04:53 +0530

    Update poll.ts
    Added ticket schema

--- models/poll.ts
+++ models/poll.ts
@@ -1,5 +1,11 @@
 import { Schema, model, models } from "mongoose";
 
+const ticketSchema = new Schema({
+	ticketId: { type: String, required: true },
+	timing: { type: String, required: true },
+	createdAt: { type: Date, default: Date.now },
+});
+
 const pollSchema = new Schema({
 	name: { type: String, required: true },
 	timings: { type: [String], required: true },
@@ -9,6 +15,7 @@ const pollSchema = new Schema({
 		enum: ["pending", "active", "ended"],
 		default: "pending",
 	},
+	tickets: { type: [ticketSchema], default: [] },
 	createdAt: { type: Date, default: Date.now },
 });
 
