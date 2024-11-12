48c74c12dabcfd97dbca7682fdd711211f75f0f0
Dhruv Anand <ananddhruv29@gmail.com>
2024-11-12 15:36:32 +0530

    Updated Poll & Ticket model
    Include `createdAt` date for the poll and ticket.

--- models/poll.ts
+++ models/poll.ts
@@ -1,12 +1,12 @@
 import { Schema, model, models } from "mongoose";
 
-const ticketSchema = new Schema({
+const ticketSchema = new Schema<Ticket>({
 	ticketId: { type: String, required: true },
 	timing: { type: String, required: true },
 	createdAt: { type: Date, default: Date.now },
 });
 
-const pollSchema = new Schema({
+const pollSchema = new Schema<Poll>({
 	name: { type: String, required: true },
 	timings: { type: [String], required: true },
 	date: { type: Date, required: true },
@@ -19,6 +19,6 @@ const pollSchema = new Schema({
 	createdAt: { type: Date, default: Date.now },
 });
 
-const Poll = models.Poll || model("Poll", pollSchema);
+const Poll = models.Poll || model<Poll>("Poll", pollSchema);
 
 export default Poll;
