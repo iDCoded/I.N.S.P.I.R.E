6a2856ef28acdb4e2bb7afc1526cee7406166723
Dhruv Anand <ananddhruv29@gmail.com>
2024-11-12 14:13:18 +0530

    Added a back button to admin panel

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,10 +1,16 @@
 import CreateNewPoll from "@/components/admin/CreatePollForm";
 import ExistingPolls from "@/components/admin/ExistingPolls";
 
+import { MoveLeft } from "lucide-react";
+import Link from "next/link";
+
 export default function AdminDashboard() {
 	return (
 		<div className="container mx-auto p-4">
-			<h1 className="text-2xl font-bold mb-4">
+			<h1 className="text-2xl font-bold mb-4 flex flex-row items-center gap-4">
+				<Link href={"/poll"}>
+					<MoveLeft />
+				</Link>
 				Bus Ticket Poll Admin Dashboard
 			</h1>
 			<CreateNewPoll />
